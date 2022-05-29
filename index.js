599d76b6f54484afef518cbd62ee46cfe0aa68dd
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-29 06:44:49 +0100

    Update

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 //concise object literal declarations using simple fields
 
+
+//ES6 introduced some nice support for easy defining object literals
+
+const createPerson = (name, age, gender) => //this is an arrow function that takes in three variables, age, name and gender, and returns an object that has series of keyvalue pairs: name, age and gender, and their values, is the passed in variables.
+{
+
+    return
+        {
+            name: name; //We can notice some repition, name, name, age, age, gender, gender. Now, the first name is the key, the second name is the value.
+            age: age;
+            gender: gender
+        };
+};
+
+console.log(createPerson("Zodiac Hasbro", 56, "male"));
+
+//There's an easy way to do create stuff like this, where the keyvalue pairs and the 
+//...variable passed are the same... See index1.js for this. 
+
+//[to be continued...]
---
+++ index1.js
@@ -0,0 +1,8 @@
+const createPerson = (name, age, gender) => ( {name, age, gender })
+console.log(createPerson("Zodiac Hasbro", 56, "male"));
+
+
+//Comment: JavaScript knows that it's going to return the objecdt by the right, and 
+//it's going to have keyvalue pairs.  
+
+//WHATEVER THAT MEANS!! {CRYING}
